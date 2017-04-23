21d6c3f0d830e553d0af8d03ec51d10b27c45ffb
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2017-04-23 16:43:13 +0200

    Add benchmark for string template vs bel toString()

---
+++ html.js
@@ -0,0 +1,35 @@
+// TODO: Handle special cases like null, undefined, etc.
+function handleValue(value) {
+  if (Array.isArray(value)) {
+    // Suppose that each item is a result of html``.
+    return value.join('')
+  }
+  // Ignore event handlers.
+  //     onclick=${(e) => doSomething(e)}
+  // will become
+  //     onclick=""
+  if (typeof value === 'function') {
+    return '""'
+  }
+  const str = value.toString()
+  return str
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;')
+}
+
+function stringify () {
+  const pieces = arguments[0]
+  let output = ''
+  for (let i = 0; i < pieces.length; i++) {
+    output += pieces[i]
+    if (i < pieces.length - 1) {
+      output += handleValue(arguments[i + 1])
+    }
+  }
+  return output
+}
+
+module.exports = stringify
