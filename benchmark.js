f1bc422a63c179eba29dfe7007bf83fa7a23088c
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2017-04-23 18:17:28 +0200

    Format benchmark result

--- benchmark.js
+++ benchmark.js
@@ -5,17 +5,20 @@ const createApp = require('./app')
 const iteration = 1000;
 
 const strApp = createApp(stringify)
+const belApp = createApp(bel)
+console.log('# stringify output')
+console.log(strApp.render().toString())
+console.log('# bel output')
+console.log(belApp.render().toString())
+console.log('# benchmark')
 console.time('stringify')
 for (let i = 0; i < iteration; i++) {
   strApp.render().toString()
 }
-console.timeEnd('stringify')
-console.log(strApp.render().toString())
+const a = console.timeEnd('stringify')
 
-const belApp = createApp(bel)
 console.time('bel')
 for (let i = 0; i < iteration; i++) {
   belApp.render().toString()
 }
 console.timeEnd('bel')
-console.log(belApp.render().toString())
