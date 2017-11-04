13a15dfd21a3a33ee4944fb1500e4564d395ab2d
Michael Jackson <mjackson@overstock.com>
2017-11-04 10:00:18 -0600

    Replace ava with tape

--- index.test.js
+++ index.test.js
@@ -2,13 +2,14 @@
 
 const createApp = require('./app')
 const pelo = require('.')
-const test = require('ava')
+const test = require('tape')
 const minify = require('html-minifier').minify
 const minifyConfig = {
   collapseWhitespace: true
 }
 
-test('app renders as expected', t => {
+test('The app renders as expected', t => {
+  t.plan(1)
   const expected = minify(createApp().render().toString(), minifyConfig)
   pelo.replace('bel')
   const actual = minify(createApp().render().toString(), minifyConfig)
